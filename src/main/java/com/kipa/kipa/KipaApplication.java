afbe8bf3e1c2d990ac6bf8f150cdc5f0112a9144
matt <mh.dev@pm.me>
2025-02-03 22:55:28 -0500

    Initial commit - SpringBoot base setup

---
+++ src/main/java/com/kipa/kipa/KipaApplication.java
@@ -0,0 +1,21 @@
+package com.kipa.kipa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class KipaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KipaApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value="name", defaultValue="World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+}
---
+++ src/test/java/com/kipa/kipa/KipaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kipa.kipa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KipaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
