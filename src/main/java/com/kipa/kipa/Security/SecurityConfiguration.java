86785cfc16a3a845879c5f47d0e5f064be1a3b98
matt <mh.dev@pm.me>
2025-02-09 14:45:50 -0500

    User auth work WIP

--- src/main/java/com/kipa/kipa/Security/SecurityConfiguration.java
+++ src/main/java/com/kipa/kipa/Security/SecurityConfiguration.java
@@ -1,12 +1,22 @@
 package com.kipa.kipa.Security;
 
+import org.aspectj.apache.bcel.classfile.Module;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -34,4 +44,49 @@ public class SecurityConfiguration {
     public WebSecurityCustomizer webSecurityCustomizer() {
         return web -> web.ignoring().requestMatchers("/images/**", "/js/**");
     }
+
+
+    /*
+        Below contains the logic for user authentication - NOTE: This will need to be refactored
+
+        Logic:
+            - User submits login credentials
+            - AuthenticationManager delegates the request to DaoAuthenticationProvider
+            - DaoAuthenticationProvider does the following:
+                    - Calls UserDetailsService to load the user from the DB
+                    - Compares hashed password to what is in the DB
+                    - Verifies the users role(s)
+            - AuthenticationManager verifies or denies the login request
+            - *** CONTINUED ***
+        _________________________________________________________________________
+         Thoroughly comment this process and the below functions when revisiting
+        _________________________________________________________________________
+    */
+
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+
+        return new ProviderManager(authenticationProvider);
+    }
+
+    // *** This will need to be heavily refactored to load real users from the DB
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("user")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
 }
