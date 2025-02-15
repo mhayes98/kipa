b8d68f2e244db9a6d4e347df4557c2b6f8a4fd5d
matt <mh.dev@pm.me>
2025-02-15 12:57:25 -0500

    Ignore restructuring / User repo

--- src/main/java/com/kipa/kipa/User/User.java
+++ src/main/java/com/kipa/kipa/User/User.java
@@ -3,6 +3,7 @@ package com.kipa.kipa.User;
 import jakarta.persistence.*;
 
 @Entity // Turns class into a database entity/table
+@Table(name="users")
 public class User {
     @Id // Primary key designation <> internal unique identifier
     @GeneratedValue(strategy=GenerationType.AUTO) // Auto-generates primary key value upon creation
@@ -27,5 +28,43 @@ public class User {
         return String.format("ID=%d | Username=%s | Email=%s", id, username, email);
     }
 
+    public long getId() {
+        return id;
+    }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
---
+++ src/main/java/com/kipa/kipa/User/UserRepository.java
@@ -0,0 +1,8 @@
+package com.kipa.kipa.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    List<User> findByRole(String role);
+}
