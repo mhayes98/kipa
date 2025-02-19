8e7dc30f0666825a3a1c740e023761e4b6407873
matt <mh.dev@pm.me>
2025-02-19 16:25:59 -0500

    DB connection testing

--- src/main/java/com/kipa/kipa/KipaApplication.java
+++ src/main/java/com/kipa/kipa/KipaApplication.java
@@ -1,5 +1,7 @@
 package com.kipa.kipa;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -12,5 +14,21 @@ public class KipaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KipaApplication.class, args);
 	}
+
+	@Value("${SPRING_DATASOURCE_URL:default_value}")
+private String datasourceUrl;
+
+@Value("${SPRING_DATASOURCE_USERNAME:default_value}")
+private String datasourceUsername;
+
+@Value("${SPRING_DATASOURCE_PASSWORD:default_value}")
+private String datasourcePassword;
+
+@PostConstruct
+public void logEnvVars() {
+    System.out.println("Datasource URL: " + datasourceUrl);
+    System.out.println("Datasource Username: " + datasourceUsername);
+    System.out.println("Datasource Password: " + datasourcePassword);
+}
 
 }
