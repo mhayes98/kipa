9ef0c320f0760aaccfa5d5fd978579a59b6d5d18
matt <mh.dev@pm.me>
2025-02-19 16:11:44 -0500

    Entity scanning

--- src/main/java/com/kipa/kipa/KipaApplication.java
+++ src/main/java/com/kipa/kipa/KipaApplication.java
@@ -2,9 +2,10 @@ package com.kipa.kipa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-
+@EntityScan(basePackages = "com.kipa.kipa.User.User")
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class KipaApplication {
 
