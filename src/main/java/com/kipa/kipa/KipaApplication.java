2bd4d1b3565b71d0342a704e63b0858b3c96d454
matt <mh.dev@pm.me>
2025-02-19 15:43:01 -0500

    Changing env. var

--- src/main/java/com/kipa/kipa/KipaApplication.java
+++ src/main/java/com/kipa/kipa/KipaApplication.java
@@ -3,8 +3,7 @@ package com.kipa.kipa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class KipaApplication {
