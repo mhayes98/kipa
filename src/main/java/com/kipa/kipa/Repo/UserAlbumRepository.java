80f04ca244caa2b74c30ef256c18ef4ae0cb364c
matt <mh.dev@pm.me>
2025-04-30 18:20:42 -0400

    Added query to match UserAlbums and Albums by username

--- src/main/java/com/kipa/kipa/Repo/UserAlbumRepository.java
+++ src/main/java/com/kipa/kipa/Repo/UserAlbumRepository.java
@@ -1,11 +1,15 @@
 package com.kipa.kipa.Repo;
 
 
+import com.kipa.kipa.Model.JoinedUserAlbumDTO;
 import com.kipa.kipa.Model.UserAlbum;
 import com.kipa.kipa.Model.UserAlbumID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +17,27 @@ public interface UserAlbumRepository extends JpaRepository<UserAlbum, UserAlbumI
     // Id clarification necessary for composite key fields
     Optional<UserAlbum> findByIdUserIDAndIdAlbumID(String userId, Integer albumId);
     boolean existsByIdAlbumID(Integer albumId);
+
+    @Query(
+        value = "SELECT " +
+            "    user_albums.albumid AS user_albumid, " +
+            "    user_albums.userid, " +
+            "    user_albums.notes, " +
+            "    user_albums.status, " +
+            "    user_albums.tags, " +
+            "    albums.albumid AS album_albumid, " +
+            "    albums.artist, " +
+            "    albums.genre, " +
+            "    albums.style, " +
+            "    albums.thumbnail, " +
+            "    albums.title, " +
+            "    albums.tracklist, " +
+            "    albums.year " +
+            "FROM user_albums " +
+            "INNER JOIN albums " +
+            "    ON albums.albumid = user_albums.albumid " +
+            "WHERE user_albums.userid = :userId",
+        nativeQuery = true)
+    List<JoinedUserAlbumDTO> findAlbumsByUser(@Param("userId") String username);
+
 }
