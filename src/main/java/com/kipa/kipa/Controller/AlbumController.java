8f8d18c75396e23d40b0c0f1f2eb56b7d2449f7b
matt <mh.dev@pm.me>
2025-04-30 18:22:46 -0400

    Removed unnecessary import

--- src/main/java/com/kipa/kipa/Controller/AlbumController.java
+++ src/main/java/com/kipa/kipa/Controller/AlbumController.java
@@ -2,10 +2,10 @@ package com.kipa.kipa.Controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.kipa.kipa.Model.Album;
-import com.kipa.kipa.Model.User;
 import com.kipa.kipa.Service.AlbumService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 
 // ****
 // API paths will need to be refactored --> Obsidian / Kipa / API Endpoint Security / Refactoring Plan
